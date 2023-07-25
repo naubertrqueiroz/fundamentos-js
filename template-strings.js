a8e9883314a1698f179e5462c2b0dce0826ab9a9
naubertrqueiroz <naubertqueiroz@gmail.com>
2023-07-25 09:51:20 -0300

    Operadores no javascript e uso de template strings

---
+++ template-strings.js
@@ -0,0 +1,10 @@
+const nome = "Naubert"
+const idade = 2023-1997;
+const cidadeDeNascimento = "Boituva";
+
+// const apresentacao = "Meu nome é : " + nome + ", minha idade é " + idade + ", nasci na cidade de " + cidadeDeNascimento + ".";
+
+const apresentacao = `Meu nome é ${nome}, minha idade ${idade}, e nasci na cidade de ${cidadeDeNascimento}.`
+
+console.log(apresentacao)
+
---
+++ ternario.js
@@ -0,0 +1,11 @@
+const idadeMinima = 18;
+const idadeCliente = 19;
+
+// if (idadeCliente >= idadeMinima) {
+//     console.log("Você pode beber cerveja")
+// } else {
+//     console.log("Você pode beber suco")
+// }
+
+            // Condição                   // True                     // False  
+console.log(idadeCliente >= idadeMinima ? "Você pode beber cerveja" : "Você pode beber suco")
