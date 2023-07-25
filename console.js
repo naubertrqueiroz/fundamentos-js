1a701d405c9147e61e1c40ebeb14574cefe52a10
naubertrqueiroz <naubertqueiroz@gmail.com>
2023-07-25 09:13:05 -0300

    Subindo arquivos sobre console e erros

---
+++ console.js
@@ -0,0 +1,10 @@
+// log _. registro
+
+// const minhaVar = true;
+// console.log(245)
+// console.log("Olá mundo")
+// console.log(minhaVar)
+
+// Tratamento de Erro!
+
+console.error("Foi encontrado um erro!")
---
+++ erros.js
@@ -0,0 +1,4 @@
+const numero = 1;
+// const minhaVar = "Hello"
+console.log(numero)
+console.log(minhaVar)
