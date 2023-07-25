f0be1994e57b0f56ac28713bbbf83a1d89db655d
naubertrqueiroz <naubertqueiroz@gmail.com>
2023-07-25 07:51:41 -0300

    Códigos var e conversões

---
+++ conversoes.js
@@ -0,0 +1,23 @@
+// tipo de dados
+// booleanos
+
+// conversão implicita
+const numero = 456;
+const numero_texto = "456";
+
+console.log(numero === numero_texto)
+console.log(numero == numero_texto)
+
+
+console.log(numero + numero_texto)
+
+// conversão explicita
+
+// Number()
+// String()
+
+
+const numero_string = Number("456");
+const numero_string_na = Number("456a");
+
+console.log(numero_string_na)
